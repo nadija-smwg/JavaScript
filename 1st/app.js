5c5c5f89cc62ddb91755dc1866ba1ac63354ce9a
Nadijasmwg <nadijasmwg@gmail.com>
2025-11-02 19:19:22 +0530

    first commit

---
+++ 1st/app.js
@@ -0,0 +1,15 @@
+function calculateShippingCost() {
+    let TotalPrice = 19;
+    //let ShippingCost;
+
+    if (TotalPrice <= 10){
+        ShippingCost = 5
+    }else if (TotalPrice <= 20){
+        ShippingCost = 3
+    } else {
+        ShippingCost = 0
+    }
+        
+        console.log(`ShippingCost for you is ${ShippingCost}`);
+
+}
---
+++ startup/start.js
@@ -0,0 +1,5 @@
+let price = "20$";  //for dynamicly changing variables
+const brand = "nike";  //for static variable 
+var name = "nadija";
+console.log(price)
+console.log("output in console")
