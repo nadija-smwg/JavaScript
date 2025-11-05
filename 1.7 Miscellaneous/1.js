be7826326cf1f08199c6e2bd89dc1c3602f352e4
Nadijasmwg <nadijasmwg@gmail.com>
2025-11-05 10:17:52 +0530

    11/5

--- 1.7 Miscellaneous/1.js
+++ 1.7 Miscellaneous/1.js
@@ -1,1 +1,33 @@
-alert("heyy user")
+document.addEventListener("click", function(){   // anonumous function
+    alert("thank you for clicking")
+})
+
+
+
+
+document.addEventListener("click", () => ("thank you for clicking"))    // aero functalert
+
+
+
+let myNumbers = [10, 50, 100]
+// let doubleNumbers = myNumbers.map(function(x){
+//     return x * 2 
+// })
+// console.log(doubleNumbers) 
+
+let doubleNumbers = myNumbers.map((x) => x * 2 )
+console.log(doubleNumbers) 
+
+
+
+let john = {
+    firstName: "john",
+    lastName: "Doe" ,
+    driveCar() {
+        let imFunction = ()=> console.log(this) 
+        imFunction()
+        console.log(this.firstName + " " + this.lastName + " is driving a car")
+    }
+}
+
+john.driveCar()
---
+++ 1.8 miscellaneous 2/1.js
@@ -0,0 +1,17 @@
+// cool()
+
+// let cool = function(){
+//     console.log("heyy")
+// }
+
+
+let myname ="nadija"
+
+console.log("hello my name is "+ myname +" and the sky is blue")
+
+// template
+console.log(`hello my name is ${myname} and the sky is blue`)
+
+
+//semicolen   (auto add semicolen (its not visible))
+let myname1 ="sudu";console.log(`hello my name is ${myname1} and the sky is blue`)
